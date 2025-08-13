905244924b441be8572956d9b4d882c575a5b1eb
Venkataram Sivaram <vesion4690@gmail.com>
2025-08-13 00:05:35 -0700

    Complete pipeline for Neural SDF

--- examples/sdf/addresses.py
+++ examples/sdf/addresses.py
@@ -1,9 +1,15 @@
+from __future__ import annotations
 import trimesh
 import slangpy as spy
+import tqdm
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import polyscope as ps
 from dataclasses import dataclass
+from scipy.ndimage import gaussian_filter
+from ..networks.addresses import Network
 from common import *
-
 
 @dataclass
 class RayFrame:
@@ -125,27 +131,44 @@ class Camera:
 
 
 class Pipeline:
-    def __init__(self, device: spy.Device):
-        SOURCE = ROOT / "examples" / "slang" / "sdf_rendering.slang"
+    @staticmethod
+    def load_specialization_module(device: spy.Device, network: Network):
+        source = f"""
+        export static const int Hidden = {network.hidden};
+        export static const int HiddenLayers = {network.hidden_layers};
+        export static const int Levels = {network.levels};
+        """
+        return device.load_module_from_source("specialization", source)
+
+    # TODO: move to common/device.py
+    @staticmethod
+    def load_pipeline(device: spy.Device, module: spy.SlangModule, extra: List[spy.SlangModule], entry_point: str):
+        program = device.link_program(
+            modules=[module] + extra,
+            entry_points=[module.entry_point(entry_point)],
+        )
+        return device.create_compute_pipeline(program)
+
+    def __init__(self, device: spy.Device, network: Network):
+        SOURCE = ROOT / "examples" / "slang" / "neural_sdf.slang"
         
         self.device = device
         self.module = device.load_module(str(SOURCE))
+        self.specialization_module = self.load_specialization_module(device, network)
 
-        # Reference rendering kernel
-        reference_entry_point = self.module.entry_point("reference")
-        reference_program = device.link_program(
-            modules=[self.module],
-            entry_points=[reference_entry_point],
-        )
-        self.reference_pipeline = device.create_compute_pipeline(reference_program)
+        self.reference_pipeline = self.load_pipeline(device, self.module, [self.specialization_module], "reference")
+        self.neural_pipeline = self.load_pipeline(device, self.module, [self.specialization_module], "neural")
+        self.sample_pipeline = self.load_pipeline(device, self.module, [self.specialization_module], "sample")
+        self.gradient_pipeline = self.load_pipeline(device, self.module, [self.specialization_module], "gradient")
+        self.optimize_pipeline = self.load_pipeline(device, self.module, [self.specialization_module], "optimize")
 
-    def render_reference(self, mesh: dict, rayframe: RayFrame, target_texture: spy.Texture):
+    def render_reference(self, mesh: TriangleMesh, rayframe: RayFrame, target_texture: spy.Texture):
         command_encoder = self.device.create_command_encoder()
 
         with command_encoder.begin_compute_pass() as cmd:
             shader_object = cmd.bind_pipeline(self.reference_pipeline)
             cursor = spy.ShaderCursor(shader_object)
-            cursor.mesh = mesh
+            cursor.mesh = mesh.dict()
             cursor.rayFrame = rayframe.dict()
             cursor.targetTexture = target_texture
             cursor.targetResolution = (target_texture.width, target_texture.height)
@@ -153,39 +176,157 @@ class Pipeline:
         
         self.device.submit_command_buffer(command_encoder.finish())
 
+    def render_neural(self, network: Network, rayframe: RayFrame, target_texture: spy.Texture):
+        command_encoder = self.device.create_command_encoder()
+
+        with command_encoder.begin_compute_pass() as cmd:
+            shader_object = cmd.bind_pipeline(self.neural_pipeline)
+            cursor = spy.ShaderCursor(shader_object)
+            cursor.network = network.dict()
+            cursor.rayFrame = rayframe.dict()
+            cursor.targetTexture = target_texture
+            cursor.targetResolution = (target_texture.width, target_texture.height)
+            cmd.dispatch(thread_count=(target_texture.width, target_texture.height, 1))
+
+        self.device.submit_command_buffer(command_encoder.finish())
+
+    def sample(self, network: Network, sample_buffer: spy.Buffer, sdf_buffer: spy.Buffer):
+        command_encoder = self.device.create_command_encoder()
+
+        with command_encoder.begin_compute_pass() as cmd:
+            shader_object = cmd.bind_pipeline(self.sample_pipeline)
+            cursor = spy.ShaderCursor(shader_object)
+            cursor.network = network.dict()
+            cursor.sampleBuffer = sample_buffer
+            cursor.sdfBuffer = sdf_buffer
+            cmd.dispatch(thread_count=(sample_buffer.size, 1, 1))
+
+        self.device.submit_command_buffer(command_encoder.finish())
+
+    def evaluate_gradient(self, network: Network, mesh: TriangleMesh, sample_buffer: spy.Buffer, loss_buffer: spy.Buffer, sample_count: int, time: int):
+        command_encoder = self.device.create_command_encoder()
+        
+        with command_encoder.begin_compute_pass() as cmd:
+            shader_object = cmd.bind_pipeline(self.gradient_pipeline)
+            cursor = spy.ShaderCursor(shader_object)
+            cursor.network = network.dict()
+            cursor.mesh = mesh.dict()
+            cursor.sampleBuffer = sample_buffer
+            cursor.lossBuffer = loss_buffer
+            cursor.lossBoost = 1.0 / sample_count
+            cursor.time = time
+            cmd.dispatch(thread_count=(sample_count, 1, 1))
+        
+        self.device.submit_command_buffer(command_encoder.finish())
+
+    def optimize(self, network: Network):
+        command_encoder = self.device.create_command_encoder()
+
+        with command_encoder.begin_compute_pass() as cmd:
+            shader_object = cmd.bind_pipeline(self.optimize_pipeline)
+            cursor = spy.ShaderCursor(shader_object)
+            cursor.network = network.dict()
+            cmd.dispatch(thread_count=(network.parameter_count, 1, 1))
+
+        self.device.submit_command_buffer(command_encoder.finish())
+
+    def layout_of(self, name: str) -> spy.TypeLayoutReflection:
+        type_layout = self.module.layout.find_type_by_name(name)
+        return self.module.layout.get_type_layout(type_layout)
+
+
+@dataclass
+class TriangleMesh:
+    vertices: spy.Buffer
+    triangles: spy.Buffer
+    triangle_count: int
+
+    @staticmethod
+    def new(device: spy.Device, mesh: trimesh.Trimesh):
+        vertices = create_buffer_32b(device, mesh.vertices.astype(np.float32), 3)
+        triangles = create_buffer_32b(device, mesh.faces.astype(np.uint32), 3)
+        return TriangleMesh(vertices, triangles, len(mesh.faces))
+
+    def dict(self):
+        return {
+            "vertices": self.vertices,
+            "triangles": self.triangles,
+            "triangleCount": self.triangle_count,
+        }
+
 
 def main():
     mesh = trimesh.load_mesh(ROOT / "resources" /"suzanne.obj")
 
-    print("vertices", mesh.vertices.shape)
-    print("faces", mesh.faces.shape)
+    print("max", mesh.vertices.max(axis=0))
+    print("min", mesh.vertices.min(axis=0))
 
     device = create_device()
     
-    # Single triangle
-    # vertex_buffer = create_buffer_32b(device, np.array((
-    #     0.0, 0.0, 0.0,
-    #     1.0, 0.0, 0.0,
-    #     0.0, 1.0, 0.0,
-    # ), dtype=np.float32), 3)
-    # face_buffer = create_buffer_32b(device, np.array((
-    #     0, 1, 2,
-    # ), dtype=np.uint32), 3)
+    network = Network(device, hidden=32, hidden_layers=2, levels=8, input=3, output=1)
+    pipeline = Pipeline(device, network)
+    mesh = TriangleMesh.new(device, mesh)
 
-    vertex_buffer = create_buffer_32b(device, mesh.vertices.astype(np.float32), 3)
-    face_buffer = create_buffer_32b(device, mesh.faces.astype(np.uint32), 3)
+    # Allocate loss buffer
+    SAMPLE_COUNT = 1 << 14
+    loss_buffer = create_buffer_32b(device, np.zeros(SAMPLE_COUNT, dtype=np.float32))
+    sample_buffer = create_buffer_32b(device, np.zeros((SAMPLE_COUNT, 3), dtype=np.float32))
 
-    mesh = {
-        "vertices": vertex_buffer,
-        "triangles": face_buffer,
-        "triangleCount": len(mesh.faces),
-        # "triangleCount": 1,
-    }
+    ps.init()
 
-    print("vertex_buffer", vertex_buffer.size)
-    print("face_buffer", face_buffer.size)
+    history = []
+    for frame in tqdm.trange(10000, desc="training"):
+        samples = 3 * (np.random.rand(SAMPLE_COUNT, 3).astype(np.float32) * 2 - 1)
+        sample_buffer.copy_from_numpy(samples)
 
-    pipeline = Pipeline(device)
+        pipeline.evaluate_gradient(network, mesh, sample_buffer, loss_buffer, SAMPLE_COUNT, frame)
+        losses = loss_buffer.to_numpy().view(np.float32)
+        history.append(losses.mean())
+
+        pipeline.optimize(network)
+
+        # samples = sample_buffer.to_numpy().view(np.float32).reshape(-1, 3)
+        # print("samples", samples.shape)
+        # print("samples", samples)
+        # cloud = ps.register_point_cloud("samples", samples)
+        # ps.show()
+
+    history = np.array(history)
+    sns.lineplot(history, alpha=0.5)
+
+    smoothed = gaussian_filter(history, 2.5)
+    sns.lineplot(smoothed, linewidth=2.5)
+
+    plt.yscale("log")
+    plt.show()
+
+    # Sample and visualize the learned SDF
+    x = np.linspace(-1, 1, 64) * 2
+    y = np.linspace(-1, 1, 64) * 2
+    z = np.linspace(-1, 1, 64) * 2
+    X, Y, Z = np.meshgrid(x, y, z, indexing="xy")
+    
+    samples = np.stack([X.flatten(), Y.flatten(), Z.flatten()], axis=1)
+    sdf = np.zeros(samples.shape[0])
+    print("np samples", samples.shape)
+    print("np sdf", sdf.shape)
+
+    samples = create_buffer_32b(device, samples.astype(np.float32), 3)
+    sdf = create_buffer_32b(device, sdf.astype(np.float32), 1)
+    
+    pipeline.sample(network, samples, sdf)
+
+    samples = samples.to_numpy().view(np.float32).reshape(-1, 3)
+    sdf = sdf.to_numpy().view(np.float32)
+    print("samples", samples.shape)
+    print("sdf", sdf.shape)
+
+    ps.init()
+    cloud = ps.register_point_cloud("samples", samples)
+    cloud.add_scalar_quantity("sdf", sdf)
+    ps.show()
+
+    # TODO: sdf query of the reference as well to test the interior...
 
     # Define consistent dimensions
     WINDOW_WIDTH = 512
@@ -202,7 +343,8 @@ def main():
     
     # Calculate aspect ratio from actual dimensions
     aspect_ratio = WINDOW_WIDTH / WINDOW_HEIGHT
-    camera = Camera.new(aspect_ratio=1)
+    camera = Camera.new(aspect_ratio=aspect_ratio)
+    camera.transform.position = np.array((0.0, 0.0, 5.0))
     
     window = spy.Window(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
     surface = device.create_surface(window)
@@ -218,6 +360,7 @@ def main():
     
     # TODO: app class with custom render loop and event handling
     frame = 0
+
     while not window.should_close():
         window.process_events()
         
@@ -226,8 +369,16 @@ def main():
         
         image = surface.acquire_next_image()
 
-        pipeline.render_reference(mesh, camera.rayframe(), texture)
-        
+        # pipeline.render_reference(mesh, camera.rayframe(), texture)
+        pipeline.render_neural(network, camera.rayframe(), texture)
+
+        # pipeline.evaluate_gradient(network, mesh, loss_buffer, SAMPLE_COUNT, frame)
+        # losses = loss_buffer.to_numpy().view(np.float32)
+        # history.append(losses.mean())
+        # print("losses", losses.mean())
+
+        # pipeline.optimize(network)
+
         command_encoder = device.create_command_encoder()
         command_encoder.blit(image, texture)
         command_encoder.set_texture_state(image, spy.ResourceState.present)
@@ -237,6 +388,8 @@ def main():
         
         frame += 1
 
-
 if __name__ == "__main__":
+    sns.set_theme()
+    sns.set_palette("pastel")
+
     main()
